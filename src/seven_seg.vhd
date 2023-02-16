72f1d47badbd5b6b2678df89d684161a5b6bb0c9
Gregory Holder <gregory.holder76@gmail.com>
2023-02-16 10:58:09 +0100

    Fixed backwards numbers

--- src/seven_seg.vhd
+++ src/seven_seg.vhd
@@ -11,18 +11,18 @@ package body seven_seg is
 
     function int_to_7_seg(val : integer RANGE 0 to 11) return std_logic_vector IS 
     BEGIN
-        case val is 
-            when 0      => return "0000001";
-            when 1      => return "1001111";
-            when 2      => return "0010010";
-            when 3      => return "0000110";
-            when 4      => return "1001100";
-            when 5      => return "0100100";
-            when 6      => return "0100000";
-            when 7      => return "0001111";
-            when 8      => return "0000001";
-            when 9      => return "0000100";
-            when others => return "1111110";
+        case val is --             GFEDCBA
+            when 0      => return "1000000";
+            when 1      => return "1111001";
+            when 2      => return "0100100";
+            when 3      => return "0110000";
+            when 4      => return "0011001";
+            when 5      => return "0010010";
+            when 6      => return "0000010";
+            when 7      => return "1111000";
+            when 8      => return "0000000";
+            when 9      => return "0010000";
+            when others => return "0111111"; -- "-" 
         END case;
     END function;
 
